2832b14149845bb9b4538be8f62733937f457f89
I_Dont_Caramel <lucaskoot@icloud.com>
2024-12-09 19:29:29 +0100

    initial commit

---
+++ meme_api_frontend/meme_api_frontendTests/meme_api_frontendTests.swift
@@ -0,0 +1,17 @@
+//
+//  meme_api_frontendTests.swift
+//  meme_api_frontendTests
+//
+//  Created by caramel on 09/12/2024.
+//
+
+import Testing
+@testable import meme_api_frontend
+
+struct meme_api_frontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
